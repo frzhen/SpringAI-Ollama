8c387b61e91e42825de98c58cbd6cc6e96ee9832
Fred Zhen <fred.zhen@gmail.com>
2024-05-13 00:33:43 +0800

    Created the endpoints for AI services

---
+++ src/main/java/guru/ysy/ollama/controllers/QuestionController.java
@@ -0,0 +1,58 @@
+package guru.ysy.ollama.controllers;
+
+import guru.ysy.ollama.model.Answer;
+import guru.ysy.ollama.model.GetCapitalRequest;
+import guru.ysy.ollama.model.Question;
+import guru.ysy.ollama.services.OllamaAiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Fred R. Zhen
+ * @Date: 2024/5/13 00:17
+ * @Email: fred.zhen@gmail.com
+ */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "Question Controller", description = "Endpoint for asking questions to Local Ollama Inference Endpoint")
+public class QuestionController {
+
+    private final OllamaAiService ollamaAiService;
+
+    @Operation(summary = "Ask any question to get an answer in string")
+    @PostMapping("/ask")
+    public ResponseEntity<Answer> askQuestion(@RequestBody Question question) {
+        return ResponseEntity.ok(ollamaAiService.getAnswer(question));
+    }
+
+    @Operation(summary = "Ask the name of a state or a country's capital")
+    @PostMapping("/capital")
+    public ResponseEntity<Answer> getCapitalName(@RequestBody GetCapitalRequest request) {
+        return ResponseEntity.ok(ollamaAiService.getCapitalByString(request));
+    }
+
+    @Operation(summary = "Ask the name of a state or a country's capital in JSON format with AI")
+    @PostMapping("/capital/json")
+    public ResponseEntity<Answer> getCapitalNameByJson(@RequestBody GetCapitalRequest request) {
+        return ResponseEntity.ok(ollamaAiService.getCapitalByJson(request));
+    }
+
+    @Operation(summary = "Ask the name of a state or a country's capital in details")
+    @PostMapping("/capital/details")
+    public ResponseEntity<Answer> getCapitalNameWithInfo(@RequestBody GetCapitalRequest request) {
+        return ResponseEntity.ok(ollamaAiService.getCapitalWithInfoByString(request));
+    }
+
+    @Operation(summary = "Ask the name of a state or a country's capital in details in JSON format with AI")
+    @PostMapping("/capital/details/json")
+    public ResponseEntity<Answer> getCapitalNameWithInfoByJson(@RequestBody GetCapitalRequest request) {
+        return ResponseEntity.ok(ollamaAiService.getCapitalWithInfoByJson(request));
+    }
+}
