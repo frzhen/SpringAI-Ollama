91daf219f0b182c3c40e25ae7ccfc167d6c72536
Fred Zhen <fred.zhen@gmail.com>
2024-05-12 10:42:30 +0800

    INIT Commit

---
+++ src/main/java/guru/ysy/ollama/OllamaApplication.java
@@ -0,0 +1,13 @@
+package guru.ysy.ollama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OllamaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/ysy/ollama/OllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.ysy.ollama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
